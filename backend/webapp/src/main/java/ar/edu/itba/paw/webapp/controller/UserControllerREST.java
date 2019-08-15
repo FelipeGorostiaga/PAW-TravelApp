69bb276ce877f7e387cdf826a6f337e55fd583db
Felipe Gorostiaga <fgorostiaga@itba.edu.ar>
2019-08-14 21:23:56 -0300

    Advances

--- backend/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/UserControllerREST.java
+++ backend/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/UserControllerREST.java
@@ -1,8 +1,10 @@
 package ar.edu.itba.paw.webapp.controller;
 
 
+import ar.edu.itba.paw.interfaces.TripService;
 import ar.edu.itba.paw.interfaces.UserPicturesService;
 import ar.edu.itba.paw.interfaces.UserService;
+import ar.edu.itba.paw.model.Trip;
 import ar.edu.itba.paw.model.User;
 import ar.edu.itba.paw.model.UserPicture;
 import ar.edu.itba.paw.webapp.dto.UserDTO;
@@ -17,12 +19,15 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 import java.net.URI;
+import java.util.List;
 import java.util.Optional;
 
 @Path("users")
 @Controller
 @Produces(value = {MediaType.APPLICATION_JSON})
 public class UserControllerREST {
+
+    public static final int DEFAULT_PAGE_SIZE = 9;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(UserControllerREST.class);
 
@@ -31,6 +36,9 @@ public class UserControllerREST {
 
     @Autowired
     private UserService us;
+
+    @Autowired
+    private TripService ts;
 
     @Context
     private UriInfo uriContext;
@@ -76,6 +84,22 @@ public class UserControllerREST {
         return Response.ok(pictureOpt.get().getPicture()).build();
     }
 
+    @GET
+    @Path("/{id}/trips")
+    public Response getUserTrips(@PathParam("id") final int id,
+                                 @DefaultValue("1") @QueryParam("page") int page,
+                                 @DefaultValue("" + DEFAULT_PAGE_SIZE) @QueryParam("per_page") int pageSize)  {
+
+        final Optional<User> userOptional = us.findById(id);
+
+        if (!userOptional.isPresent()) {
+            LOGGER.debug("Failed to get user with ID: {} trips, user not found", id);
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+        User user = userOptional.get();
+        final List<Trip> trips = ts.getAllUserTrips(user);
+        return Response.ok(trips).build();
+    }
 
 
 }
