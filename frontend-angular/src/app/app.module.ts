40fc621c6b0e8e4a55ed0468a4cd71d68c2ef074
Felipe Gorostiaga <fgorostiaga@itba.edu.ar>
2020-07-02 18:18:52 -0300

    Added component structures and icon

--- frontend-angular/src/app/app.module.ts
+++ frontend-angular/src/app/app.module.ts
@@ -4,11 +4,31 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { TripComponent } from './trip/trip.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AboutComponent } from './about/about.component';
+import { InformationComponent } from './trip/information/information.component';
+import { ActivitiesComponent } from './trip/activities/activities.component';
+import { ChatComponent } from './trip/chat/chat.component';
+import { CommentComponent } from './trip/chat/comment/comment.component';
+import { MapComponent } from './map/map.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    TripComponent,
+    LoginComponent,
+    RegisterComponent,
+    AboutComponent,
+    InformationComponent,
+    ActivitiesComponent,
+    ChatComponent,
+    CommentComponent,
+    MapComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
