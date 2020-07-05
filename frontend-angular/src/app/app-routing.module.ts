949b000114f80251f98c0c53a3ed9349f0ed7066
Felipe Gorostiaga <fgorostiaga@itba.edu.ar>
2020-07-05 17:44:08 -0300

    API user service methods

--- frontend-angular/src/app/app-routing.module.ts
+++ frontend-angular/src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import {LoginComponent} from './login/login.component';
 import {RegisterComponent} from './register/register.component';
 import {HomeComponent} from './home/home.component';
+import {TripComponent} from './trip/trip.component';
 
 
 const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent}
-
+  {path: 'register', component: RegisterComponent},
+  {path: 'trip/:id', component: TripComponent}
 ];
 
 @NgModule({
--- frontend-angular/src/app/services/api-user.service.ts
+++ frontend-angular/src/app/services/api-user.service.ts
@@ -1,5 +1,11 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
+import {Trip} from '../model/trip';
+import {UserAuth} from '../model/user-auth';
+import {User} from '../model/user';
+import {UserForm} from '../model/user-form';
+
 
 
 @Injectable({
@@ -7,11 +13,40 @@ import {HttpClient} from '@angular/common/http';
 })
 export class ApiUserService {
 
-  constructor(private http: HttpClient) { }
+    private BASE_URL = 'http://localhost:8080/api';
+    private USERS_BASE_URL = `${this.BASE_URL}/users/`;
+    public CREATE_USER_URL = `${this.BASE_URL}/create`;
+    public AUTHENTICATE_URL = `${this.BASE_URL}/users/authenticate`;
+
+    constructor(private http: HttpClient) {}
+
+    // ------------------------ Users ---------------------
+
+    getUserTrips(id: string, page: number): Observable<Trip[]> {
+        const url = this.USERS_BASE_URL + id  + '/trips';
+        return this.http.get<Trip[]>(url);
+    }
+
+    authenticateUser(userAuth: UserAuth): Observable<any>  {
+        return this.http.post(this.AUTHENTICATE_URL, userAuth);
+    }
 
 
-  getUserTrips(): any {
-    return this;
-  }
+    getUserById(id: string): Observable<User> {
+        const url = this.BASE_URL + '/users/' + id;
+        return this.http.get<User>(url);
+    }
+
+    createUser(userForm: UserForm): Observable<User> {
+        return this.http.post<User>(this.CREATE_USER_URL, userForm);
+    }
+
+    getUserPicture(id: string): Observable<any> {
+        const url = this.BASE_URL + '/users/' + id + '/picture';
+        return this.http.get(url);
+    }
+
+    // ------------------------------------------------
+
 
 }
