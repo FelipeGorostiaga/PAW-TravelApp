1d37772dae1748fe70758627aafe338bd5131c33
Felipe Gorostiaga <fgorostiaga@itba.edu.ar>
2021-02-07 21:48:05 -0300

    Trip card date pipe

--- frontend-angular/src/app/home/trip-card/trip-card.component.ts
+++ frontend-angular/src/app/home/trip-card/trip-card.component.ts
@@ -2,49 +2,57 @@ import {Component, Input, OnInit} from '@angular/core';
 import {Trip} from "../../model/trip";
 import {ApiUserService} from "../../services/api-user.service";
 import {ApiTripService} from "../../services/api-trip.service";
-import { Router } from '@angular/router';
+import {Router} from '@angular/router';
 import {DomSanitizer} from "@angular/platform-browser";
+import {DateUtilService} from "../../services/date-util.service";
 
 @Component({
-  selector: 'app-trip-card',
-  templateUrl: './trip-card.component.html',
-  styleUrls: ['./trip-card.component.scss']
+    selector: 'app-trip-card',
+    templateUrl: './trip-card.component.html',
+    styleUrls: ['./trip-card.component.scss']
 })
 export class TripCardComponent implements OnInit {
 
-  @Input() trip: Trip;
+    @Input() trip: Trip;
 
-  hasImage;
-  tripImage: any;
-  loadingImage;
+    hasImage;
+    tripImage: any;
+    loadingImage;
 
-  constructor(private tripService: ApiTripService,
-              private sanitizer: DomSanitizer,
-              private router: Router) { }
+    startDate: Date;
+    endDate: Date;
 
-  ngOnInit() {
-    this.loadingImage = true;
-    this.tripService.getTripCardImage(this.trip.id).subscribe(
-        data => {
-          const reader = new FileReader();
-          reader.onload = (e) => {
-            // @ts-ignore
-            this.tripImage = this.sanitizer.bypassSecurityTrustUrl(e.target.result);
-            this.loadingImage = false;
-            this.hasImage = true;
-          }
-          reader.readAsDataURL(new Blob([data]));
-        },
-        error => {
-          this.loadingImage = false;
-          this.hasImage = false;
-        }
-    );
-  }
+    constructor(private tripService: ApiTripService,
+                private sanitizer: DomSanitizer,
+                private router: Router,
+                private dateUtil: DateUtilService) {
+    }
 
-  navigateToTrip() {
-    const url = '/trip/' + this.trip.id;
-    this.router.navigate([url])
-  }
+    ngOnInit() {
+        this.startDate = this.dateUtil.stringToDate(this.trip.startDate);
+        this.endDate = this.dateUtil.stringToDate(this.trip.endDate);
+        this.loadingImage = true;
+        this.tripService.getTripCardImage(this.trip.id).subscribe(
+            data => {
+                const reader = new FileReader();
+                reader.onload = (e) => {
+                    // @ts-ignore
+                    this.tripImage = this.sanitizer.bypassSecurityTrustUrl(e.target.result);
+                    this.loadingImage = false;
+                    this.hasImage = true;
+                };
+                reader.readAsDataURL(new Blob([data]));
+            },
+            error => {
+                this.loadingImage = false;
+                this.hasImage = false;
+            }
+        );
+    }
+
+    navigateToTrip() {
+        const url = '/trip/' + this.trip.id;
+        this.router.navigate([url]);
+    }
 
 }
