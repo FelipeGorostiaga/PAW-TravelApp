a0e0cb4bad3bfd6af875977f27bd89b4cddce898
Felipe Gorostiaga <fgorostiaga@itba.edu.ar>
2021-02-11 21:06:17 -0300

    Advanced search (WIP)

--- frontend-angular/src/app/advanced-search/advanced-search.component.ts
+++ frontend-angular/src/app/advanced-search/advanced-search.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ApiSearchService} from "../services/api-search.service";
+import {NgxSpinnerService} from "ngx-bootstrap-spinner";
+import {BsDatepickerConfig} from "ngx-bootstrap/datepicker";
 
 @Component({
   selector: 'app-advanced-search',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdvancedSearchComponent implements OnInit {
 
-  constructor() { }
+
+  startDate: Date;
+  endDate: Date;
+  nameInput;
+  placeInput;
+
+  bsConfig: Partial<BsDatepickerConfig> = Object.assign({}, {containerClass: 'theme-dark-blue', dateInputFormat: 'DD/MM/YYYY'});
+
+  constructor(private searchService: ApiSearchService,
+              private spinner: NgxSpinnerService) { }
 
   ngOnInit(): void {
+
+    this.spinner.show();
+
+    this.spinner.hide();
   }
 
+    searchTrips() {
+        console.log("search trips");
+    }
 }
